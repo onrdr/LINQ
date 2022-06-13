33af8f81feb99e662574f98f90b7846c39fd3869
ONUR DERMAN <onrdrmn@gmail.com>
2022-06-13 23:03:08 +0300

    Add project files.

---
+++ LINQ_Person/ConsoleUI/Program.cs
@@ -0,0 +1,68 @@
+﻿using LinqLibrary;
+
+
+List<Person> people = ListManager.LoadSampleData();
+
+#region Order By
+// Order By Last Name Ascending
+var people1 = people.OrderBy(x => x.LastName).ToList();
+Print("Order by LastName ", people1);
+
+// Order By Last Name Descending
+var people2 = people.OrderByDescending(x => x.LastName).ToList();
+Print("Order by LastName descending ", people2);
+
+// First Order By LastName Descending and then Order By YearsExperience Ascending
+var people3 = people.OrderByDescending(x => x.LastName).ThenBy(x => x.YearsExperience).ToList();
+Print("Order by LastName descending and then YearsExperience ascending ", people3);
+
+// First Order By LastName Descending and then Order By YearsExperience Descending 
+var people4 = people.OrderByDescending(x => x.LastName).ThenByDescending(x => x.YearsExperience).ToList();
+Print("Order by LastName descending and then YearsExperience descending ", people4);
+#endregion
+
+#region Where
+// List of people that has YearExperince less than 9
+var people5 = people.Where(x => x.YearsExperience < 9).ToList();
+Print("Using where with experience smaller than 9", people5);
+
+
+// List of people that has YearExperince more than 9 and borned in march
+var people6 = people.Where(x => x.YearsExperience > 9 && x.Birthday.Month == 3).ToList();
+Print("Using where with experience bigger than 9 and birthday month is march", people6);
+#endregion
+
+#region Sum
+// The sum of the YearsExperience of all the people in the people list 
+int yearsTotal = people.Sum(x => x.YearsExperience);
+Console.WriteLine($"\nThe total years of experince of all the people is {yearsTotal}");
+ 
+// The sum of the YearsExperience of all the people who borned in march (chained methods => where and sum)
+yearsTotal = people.Where(x => x.Birthday.Month == 3).Sum(x => x.YearsExperience); 
+Console.WriteLine($"\nThe total years of experince of the people who borned in march is {yearsTotal}");
+#endregion
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine();
+
+static void Print(string heading, List<Person> list)
+{
+    Console.WriteLine($"\n{heading}");
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    foreach (var person in list)
+    {
+        Console.WriteLine($"{person.FirstName} {person.LastName} ({person.Birthday.ToShortDateString()}) : Experience {person.YearsExperience}");
+    }
+    Console.ForegroundColor = ConsoleColor.Gray;
+}
+
+Console.ReadLine();
---
+++ LINQ_Person/LinqLibrary/ListManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLibrary
+{
+    public class ListManager
+    {
+        public static List<Person> LoadSampleData()
+        {
+            List<Person> output = new()
+            {
+                new Person { FirstName = "Onur", LastName = "Derman", Birthday = Convert.ToDateTime("20/1/1987"), YearsExperience = 12},
+                new Person { FirstName = "Yeliz", LastName = "Koç", Birthday = Convert.ToDateTime("21/2/2003"), YearsExperience = 2},
+                new Person { FirstName = "Mehmet", LastName = "Kara", Birthday = Convert.ToDateTime("22/3/1977"), YearsExperience = 23},
+                new Person { FirstName = "Ali", LastName = "Çelik", Birthday = Convert.ToDateTime("23/4/1981"), YearsExperience = 16},
+                new Person { FirstName = "Gamze", LastName = "Adalı", Birthday = Convert.ToDateTime("24/5/1992"), YearsExperience = 8},
+                new Person { FirstName = "Mine", LastName = "Kara", Birthday = Convert.ToDateTime("25/6/2002"), YearsExperience = 4}
+            };
+
+            return output;
+        } 
+    }
+}
