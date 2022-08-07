a18c9e34a6fe917a33e05cc6ac183a5f2a1915b7
onrdrmn <onrdrmn@gmail.com>
2022-08-07 15:20:37 +0300

    Curve Variable added.

--- DisplayStudentsGrades/Program.cs
+++ DisplayStudentsGrades/Program.cs
@@ -8,8 +8,9 @@ var list = new List<Student>()
     new Student () { FullName = "Ziya", Gender = 'M', Midterm = 80, Final = 70 },
     new Student () { FullName = "İsmail", Gender = 'M', Midterm = 90, Final = 80 },
     new Student () { FullName = "Büşra", Gender = 'F', Midterm = 100, Final = 90 }
-};  
+};
 
+float curve = list.Sum(s=> s.Average) / list.Count;
 
 Console.WriteLine("All Student List Ordered By Full Name:");
 list.OrderBy(f => f.FullName).ToList().ForEach(s => DisplayInfo(s)); 
@@ -21,22 +22,22 @@ Console.WriteLine("\nMale Students List :");
 list.Where(ms => ms.Gender == 'M').ToList().ForEach(ms => DisplayInfo(ms)); 
 
 Console.WriteLine("\nPassed List:");
-list.Where(ps => ps.Average >= 75).ToList().ForEach(ps => DisplayInfo(ps)); 
+list.Where(ps => ps.Average >= curve).ToList().ForEach(ps => DisplayInfo(ps)); 
 
 Console.WriteLine("\nFailed List :");
-list.Where(fs => fs.Average < 75).ToList().ForEach(fs => DisplayInfo(fs)); 
+list.Where(fs => fs.Average < curve).ToList().ForEach(fs => DisplayInfo(fs)); 
 
 Console.WriteLine("\nPassed Female List:");
-list.Where(pfs => pfs.Average >= 75 && pfs.Gender == 'F').ToList().ForEach(pfs => DisplayInfo(pfs)); 
+list.Where(pfs => pfs.Average >= curve && pfs.Gender == 'F').ToList().ForEach(pfs => DisplayInfo(pfs)); 
 
 Console.WriteLine("\nPassed Male List:");
-list.Where(pms => pms.Average >= 75 && pms.Gender == 'M').ToList().ForEach(pms => DisplayInfo(pms)); 
+list.Where(pms => pms.Average >= curve && pms.Gender == 'M').ToList().ForEach(pms => DisplayInfo(pms)); 
 
 Console.WriteLine("\nFailed Female List :");
-list.Where(ffs => ffs.Average < 75 && ffs.Gender == 'F').ToList().ForEach(ffs => DisplayInfo(ffs)); 
+list.Where(ffs => ffs.Average < curve && ffs.Gender == 'F').ToList().ForEach(ffs => DisplayInfo(ffs)); 
 
 Console.WriteLine("\nFailed Male List :");
-list.Where(fms => fms.Average < 75 && fms.Gender == 'M').ToList().ForEach(fms => DisplayInfo(fms));  
+list.Where(fms => fms.Average < curve && fms.Gender == 'M').ToList().ForEach(fms => DisplayInfo(fms));  
 
 
 
