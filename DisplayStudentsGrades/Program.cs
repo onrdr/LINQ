da5bcfae422fd6fec08a140db6df2922e5b2a7b8
onrdrmn <onrdrmn@gmail.com>
2022-08-07 15:14:22 +0300

    Char value name changed and DisplayInfo method added.

--- DisplayStudentsGrades/Program.cs
+++ DisplayStudentsGrades/Program.cs
@@ -2,67 +2,48 @@
 
 var list = new List<Student>()
 {
-    new Student () { FullName = "Onur", Sex = 'M', Midterm = 50, Final = 40 },
-    new Student () { FullName = "Kübra", Sex = 'F', Midterm = 60, Final = 50 },
-    new Student () { FullName = "Celalettin", Sex = 'M', Midterm = 70, Final = 60 },
-    new Student () { FullName = "Ziya", Sex = 'M', Midterm = 80, Final = 70 },
-    new Student () { FullName = "İsmail", Sex = 'M', Midterm = 90, Final = 80 },
-    new Student () { FullName = "Büşra", Sex = 'F', Midterm = 100, Final = 90 }
-}; 
+    new Student () { FullName = "Onur", Gender = 'M', Midterm = 50, Final = 40 },
+    new Student () { FullName = "Kübra", Gender = 'F', Midterm = 60, Final = 50 },
+    new Student () { FullName = "Celalettin", Gender = 'M', Midterm = 70, Final = 60 },
+    new Student () { FullName = "Ziya", Gender = 'M', Midterm = 80, Final = 70 },
+    new Student () { FullName = "İsmail", Gender = 'M', Midterm = 90, Final = 80 },
+    new Student () { FullName = "Büşra", Gender = 'F', Midterm = 100, Final = 90 }
+};  
 
-// ****************************************************
+
 Console.WriteLine("All Student List Ordered By Full Name:");
-list.OrderBy(f => f.FullName)
-    .ToList()
-    .ForEach(s => Console.WriteLine(s.ToString()));
-// ****************************************************
+list.OrderBy(f => f.FullName).ToList().ForEach(s => DisplayInfo(s)); 
 
 Console.WriteLine("\nFemale Students List :");
-list.Where(f => f.Sex == 'F')
-    .ToList()
-    .ForEach(f => Console.WriteLine(f.ToString()));
-// ****************************************************
+list.Where(fs => fs.Gender == 'F').ToList().ForEach(fs => DisplayInfo(fs)); 
 
 Console.WriteLine("\nMale Students List :");
-list.Where(m => m.Sex == 'M')
-    .ToList()
-    .ForEach(m => Console.WriteLine(m.ToString()));
-// ****************************************************
+list.Where(ms => ms.Gender == 'M').ToList().ForEach(ms => DisplayInfo(ms)); 
 
 Console.WriteLine("\nPassed List:");
-list.Where(s => s.Average >= 75)
-    .ToList()
-    .ForEach(s => Console.WriteLine(s.ToString()));
-// ****************************************************
+list.Where(ps => ps.Average >= 75).ToList().ForEach(ps => DisplayInfo(ps)); 
 
 Console.WriteLine("\nFailed List :");
-list.Where(s => s.Average < 75)
-    .ToList()
-    .ForEach(s => Console.WriteLine(s.ToString()));
-// ****************************************************
+list.Where(fs => fs.Average < 75).ToList().ForEach(fs => DisplayInfo(fs)); 
 
 Console.WriteLine("\nPassed Female List:");
-list.Where(s => s.Average >= 75 && s.Sex == 'F')
-    .ToList()
-    .ForEach(s => Console.WriteLine(s.ToString()));
-// ****************************************************
+list.Where(pfs => pfs.Average >= 75 && pfs.Gender == 'F').ToList().ForEach(pfs => DisplayInfo(pfs)); 
 
 Console.WriteLine("\nPassed Male List:");
-list.Where(s => s.Average >= 75 && s.Sex == 'M')
-    .ToList()
-    .ForEach(s => Console.WriteLine(s.ToString()));
-// ****************************************************
+list.Where(pms => pms.Average >= 75 && pms.Gender == 'M').ToList().ForEach(pms => DisplayInfo(pms)); 
 
 Console.WriteLine("\nFailed Female List :");
-list.Where(s => s.Average < 75 && s.Sex == 'F')
-    .ToList()
-    .ForEach(s => Console.WriteLine(s.ToString()));
-// ****************************************************
+list.Where(ffs => ffs.Average < 75 && ffs.Gender == 'F').ToList().ForEach(ffs => DisplayInfo(ffs)); 
 
 Console.WriteLine("\nFailed Male List :");
-list.Where(s => s.Average < 75 && s.Sex == 'M')
-    .ToList()
-    .ForEach(s => Console.WriteLine(s.ToString()));
-// ****************************************************
- 
+list.Where(fms => fms.Average < 75 && fms.Gender == 'M').ToList().ForEach(fms => DisplayInfo(fms));  
+
+
+
 Console.ReadKey();
+
+
+static void DisplayInfo(Student s)
+{
+    Console.WriteLine(s.ToString());
+}
--- DisplayStudentsGrades/Student.cs
+++ DisplayStudentsGrades/Student.cs
@@ -1,7 +1,7 @@
 ﻿ class Student
 {
     public string FullName { get; set; }
-    public char Sex { get; set; }
+    public char Gender { get; set; }
     public float Midterm { get; set; }
     public float Final { get; set; }
     public float Average => Midterm * 0.4F + Final * 0.6F; 
